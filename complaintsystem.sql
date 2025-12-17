175c3ebcc52e2284cdd96b0307f323addee1e2f0
PHENOMVALENCE <phenomenalvalence@gmail.com>
2025-12-17 12:52:57 +0300

    database

--- complaintsystem.sql
+++ complaintsystem.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 15, 2024 at 02:48 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+-- Generation Time: Dec 17, 2025 at 10:52 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -44,7 +44,7 @@ INSERT INTO `complaints` (`complaint_id`, `student_username`, `complaint`, `stat
 (4, 'Idris@Gmail.com', 'Hakuna mwalimu', 'resolved', 'Poa', '2024-01-09 11:40:30'),
 (6, 'Valence', 'Uneven food distribution in the DH', 'pending', NULL, '2024-01-15 05:06:44'),
 (7, 'Idris@Gmail.com', 'Car parking slots', 'pending', NULL, '2024-01-15 05:07:29'),
-(8, 'Allan', 'Valence Anampenda Natchie', 'resolved', 'Mwambie ale show', '2024-01-15 13:16:40');
+(10, 'Nargis', 'Food shortages', 'resolved', 'Food has been ordered', '2025-12-17 05:29:11');
 
 -- --------------------------------------------------------
 
@@ -66,13 +66,14 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`user_id`, `username`, `password`, `role`, `approved`) VALUES
 (1, 'Valence', '$2y$10$59yhYNBiSNPTaTkDikjyM.Xo1KwgskkiBqmh5xVCZg6qAUpujBOPO', 'student', 0),
-(3, 'Rudolf', '$2y$10$JIH.gAT.mcuGQEyCxB9roujUfU2ClhhuC1Nf25jFmxlhmsW7coA/a', 'teacher', 0),
+(3, 'Rudolf', '$2y$10$JIH.gAT.mcuGQEyCxB9roujUfU2ClhhuC1Nf25jFmxlhmsW7coA/a', 'teacher', 1),
 (4, 'Admin', '$2y$10$iTj3YkBLuINxSzU4NXs1jOTGjrG7K4dXqKc6hv7eaU2q78hkVa8cO', 'admin', 0),
 (5, 'Idris@Gmail.com', '$2y$10$2cg2OgyvdK.1wXLE8o1l..rQH6MX0LSLSWu3OsIpZYSR2jwnM719m', 'student', 0),
-(8, 'Robin', '$2y$10$XUzQbsqsycseivC4nzfIb.wvQZqNJZryaljBB8G/0qGTTcVGeut9y', 'teacher', 0),
 (10, 'RobinHood', '$2y$10$zN8xNbmmtcT3rW3vYdRxCu2tfMndBULHF8LJQeVucpEi6Is1XiLei', 'teacher', 0),
 (11, 'RobinHood20', '$2y$10$.bnEUpq.iq2csNga.ALWoea6XCpY8dBgXs5Ynu5hXKq.sHtCChaTu', 'teacher', 0),
-(12, 'Allan', '$2y$10$upr0ykKTmaFWf8cB2g9lY.T7SE9vqU94RsLxlfkw41p7buYdkN/rW', 'student', 0);
+(12, 'Allan', '$2y$10$upr0ykKTmaFWf8cB2g9lY.T7SE9vqU94RsLxlfkw41p7buYdkN/rW', 'student', 0),
+(13, 'Nargis', '$2y$10$UjesU8SBi/YHDJAE4YrOoOecdDW4RR2Nuk798g1DcuLsTvbUOcDLq', 'student', 1),
+(14, 'Betty', '$2y$10$aAmc/P8OGZy.SDHbxN2BkuuDJrZKpTtNK6.95bDAdbhc/q28MvvPi', 'teacher', 1);
 
 --
 -- Indexes for dumped tables
@@ -100,13 +101,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `complaints`
 --
 ALTER TABLE `complaints`
-  MODIFY `complaint_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `complaint_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Constraints for dumped tables
